6e9bcca293dbe3b002fc6214c26779466d985802
郑媛 <zhengyuan@vbhledger.com>
2021-09-27 16:31:22 +0800

    降序返回第一条

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = express()
 server.get('/taf/api/downloadUrl', (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json;charset=utf-8');
-    let sql = 'SELECT * FROM distribute'
+    let sql = 'SELECT * FROM distribute order by id desc limit 1'
     db.query(sql, function (result) {
         res.send(JSON.stringify(result));
     })
